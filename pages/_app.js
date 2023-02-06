be934b54256d81dfe2923b483e63b0f70c11419b
ismat27 <adegbiteismail27@gmail.com>
2023-02-06 13:37:52 -0800

    wrapped app with common layout component

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
 import DarkModeProvider from '@/context/dark_mode'
-import Header from '@/components/Header'
+import Layout from '@/components/Layout'
 
 export default function App({ Component, pageProps }) {
   return (
     <DarkModeProvider>
-      <Header />
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </DarkModeProvider>
   )
 }
