ae80e38a4c130dc441e44838ef19c3686a1ab5dd
ismat27 <adegbiteismail27@gmail.com>
2023-02-06 07:26:55 -0800

    add header component

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,11 @@
 import '@/styles/globals.css'
 import DarkModeProvider from '@/context/dark_mode'
+import Header from '@/components/Header'
 
 export default function App({ Component, pageProps }) {
   return (
     <DarkModeProvider>
+      <Header />
       <Component {...pageProps} />
     </DarkModeProvider>
   )
