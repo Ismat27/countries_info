2e0ebb1be76cdd97b427e33405852da176b6487a
ismat27 <adegbiteismail27@gmail.com>
2023-02-07 09:37:08 -0800

    rearrange countries on the home page

--- pages/index.js
+++ pages/index.js
@@ -1,17 +1,27 @@
 import Head from 'next/head'
-import CountryCard from '@/components/CountryCard'
+import ContinentBlock from '@/components/ContinentBlock'
 
 export async function getStaticProps() {
   const response = await fetch('https://restcountries.com/v2/all')
   const data = await response .json()
+  let continents = new Set(data.map((countryData) => {
+    return countryData.region
+  }))
+  continents = [...continents]
+  const continents_data = continents.map((continent => {
+    const continent_countries = data.filter(countryData => countryData.region === continent)
+    return {
+      [continent]: continent_countries.slice(0, 4),
+    }
+  }))
   return {
     props: {
-      data
+      continents_data
     }
   }
 }
 
-export default function Home({data}) {
+export default function Home({ continents_data }) {
   return (
     <>
       <Head>
@@ -21,15 +31,22 @@ export default function Home({data}) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={''}>
-          <div className='countries'>
-            {
-              data.map(countryData => {
-                return (
-                  <CountryCard key={countryData.name} dt={countryData} />
-                )
-              })
-            }
-          </div>
+        <div className='page-center'>
+          {
+            continents_data.map(continent => {
+              let temp = Object.entries(continent)
+              temp = [...temp[0]]
+              const [continent_name, continent_data] = temp
+              return (
+                <ContinentBlock 
+                  key={continent_name} 
+                  continent_name={continent_name} 
+                  continent_data={continent_data} 
+                />
+              )
+            })
+          }
+        </div>
       </main>
     </>
   )
