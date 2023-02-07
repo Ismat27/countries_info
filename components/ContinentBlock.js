792a2523ebdde07da7c77cd1d816719bfb34cfd2
ismat27 <adegbiteismail27@gmail.com>
2023-02-07 09:36:18 -0800

    component to display group of countries in the same continent

---
+++ components/ContinentBlock.js
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import CountryCard from './CountryCard'
+
+const ContinentBlock = ({continent_name, continent_data}) => {
+  return (
+    <div className='continent_block'>
+        <div className='continent_block_header'>
+            <h2>{continent_name}</h2>
+            <Link href={'/'}>See all</Link>
+        </div>
+        <div className='countries'>
+            {
+                continent_data.map(countryData => {
+                    return (
+                        <CountryCard key={countryData.name} dt={countryData} />
+                    )
+                })
+            }
+        </div>
+    </div>
+  )
+}
+
+export default ContinentBlock
