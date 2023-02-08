e92ba19c2493583c666de0a40a585ca10b2e50e0
ismat27 <adegbiteismail27@gmail.com>
2023-02-08 12:45:51 -0800

    added search and filter component to the layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import { useDarkMode } from '@/context/dark_mode'
 import Header from './Header'
+import SearchFilter from './SearchFilter'
 
 const Layout = ({children}) => {
   const {darkMode} = useDarkMode()
   return (
     <div className={`body ${darkMode? 'bbg-dark': 'bbg-white'}`}>
         <Header />
+        <SearchFilter />
         {children}
     </div>
   )
